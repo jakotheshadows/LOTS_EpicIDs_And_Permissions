232860fc4461eb9aeeb2d4f86054a3b530615653
jakotheshadows <wolf@mtu.edu>
2017-05-12 11:13:23 -0400

    Add project files.

---
+++ LOTS_EpicIDs_And_Permissions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOTS_EpicIDs_And_Permissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
